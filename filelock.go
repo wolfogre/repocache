4d12bea2d525889f766dc58f31ee2dcbd8da0423
Jason Song <i@wolfogre.com>
2017-08-11 17:22:50 +0800

    First commit

---
+++ filelock.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"sync"
+)
+
+var filelock = &FileLock{
+	status: make(map[string]byte),
+	mutex: sync.Mutex{},
+}
+
+type FileLock struct {
+	status map[string]byte
+	mutex sync.Mutex
+}
+
+func (fl *FileLock) RLock(path string) bool {
+	fl.mutex.Lock()
+	defer fl.mutex.Unlock()
+	if v, ok := fl.status[path]; ok {
+		if v != 0 {
+			v++
+			fl.status[path] = v
+			return true
+		}
+		return false
+	}
+	fl.status[path] = 1
+	return true
+}
+
+func (fl *FileLock) RUnlock(path string) {
+	fl.mutex.Lock()
+	defer fl.mutex.Unlock()
+	if v, ok := fl.status[path]; ok {
+		if v != 0 {
+			v--
+			if v == 0 {
+				delete(fl.status, path)
+			} else {
+				fl.status[path] = v
+			}
+			return
+		}
+		panic("RUnlock a WLock")
+	}
+	panic("RUnlock before RLock")
+}
+
+func (fl *FileLock) WLock(path string) bool {
+	fl.mutex.Lock()
+	defer fl.mutex.Unlock()
+	if _, ok := fl.status[path]; ok {
+		return false
+	}
+	fl.status[path] = 0
+	return true
+}
+
+func (fl *FileLock) WUnlock(path string) {
+	fl.mutex.Lock()
+	defer fl.mutex.Unlock()
+	if v, ok := fl.status[path]; ok {
+		if v == 0 {
+			delete(fl.status, path)
+			return
+		}
+		panic("WUnlock a RLock")
+	}
+	panic("WUnlock before WLock")
+}
